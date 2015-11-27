1efea173fb765aa9e74555f9589b39cd0d0110bb
Matthew Kenzie <matthew.william.kenzie@cern.ch>
2015-11-27 13:03:12 +0100

    Add LinkDef

---
+++ roofit/include/LinkDef.h
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
+#pragma link C++ class RooExpAndGauss+;
+#pragma link C++ class RooIpatia2+;
+#pragma link C++ class RooPhysBkg+;
+
+#endif
+
